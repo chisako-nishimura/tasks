8bb50f69af11ced1bf2211042ea75fdec8011573
Chisako Nishimura <chisaco0403+test@gmail.com>
2020-10-11 18:59:53 +0900

    外からのサインアップ機能は使えなくする

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :tasks
-  devise_for :users
+  devise_for :users, skip: [:registrations]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#index'
   get 'pages/show'
