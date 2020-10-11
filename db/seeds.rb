637bf27c08ef5477ae3525a317ef3f9fdd653436
Chisako Nishimura <chisaco0403+test@gmail.com>
2020-10-11 19:00:08 +0900

    初期ユーザーのseed追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.new(email: 'init@example.com', username: '初期ユーザー', password: 'Passw0rd')
+user.save!
