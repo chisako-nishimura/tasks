edd2dabc23567f26b1811bd3fdc0884c71f5c03c
Chisako Nishimura <chisaco0403+test@gmail.com>
2020-10-11 18:45:45 +0900

    アカウント登録で、ユーザー名を入れられるようにする

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#index'
   get 'pages/show'
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
